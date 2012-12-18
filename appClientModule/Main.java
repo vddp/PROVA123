68f883031545e6d092777ec253133211fddc62b6
Valerio D. Di Paolo <valeriodipa@hotmail.com>
2012-12-18 18:55:37 +0100

    commit

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -2,12 +2,14 @@
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("STAMPA");
 	}
 
 	/* (non-Java-doc)
 	 * @see java.lang.Object#Object()
 	 */
 	public Main() {
+		
 		super();
 	}
 
