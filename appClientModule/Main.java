59ce092f18aa35cc68d8c7d8fe27e5d8c8dbaec3
Valerio D. Di Paolo <valeriodipa@hotmail.com>
2012-12-18 18:58:52 +0100

    msg

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("STAMPA");
-		System.out.println("STAMPA2");
+		System.out.println("STAMPA222");
 	}
 
 	/* (non-Java-doc)
